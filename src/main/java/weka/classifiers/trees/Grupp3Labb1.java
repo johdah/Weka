f71c4d8a2556e82659b89aff465686b9ce8dc82f
Johan Dahlberg <info@johandahlberg.com>
2013-02-16 16:02:25 +0100

    Renaming splitIndex

--- src/main/java/weka/classifiers/trees/Grupp3Labb1.java
+++ src/main/java/weka/classifiers/trees/Grupp3Labb1.java
@@ -125,7 +125,7 @@ public class Grupp3Labb1
     private boolean m_UseBinarySplits;
 
     int m_NumberOfSplits = 3;
-    private double[] splitIndex;
+    private double[] splitValues;
 
     /**
      * Returns a string describing the classifier.
@@ -221,7 +221,7 @@ public class Grupp3Labb1
      * @exception Exception if decision tree can't be built successfully
      */
     private void makeTree(Instances data) throws Exception {
-        splitIndex = new double[data.numAttributes()-1];
+        splitValues = new double[data.numAttributes()-1];
 
         // Check if no instances have reached this node.
         if (data.numInstances() == 0) {
@@ -338,25 +338,20 @@ public class Grupp3Labb1
 
             if(m_UseBinarySplits) {
                 if(m_Attribute.isNumeric()) {
-                    int value = (int) instance.value(m_Attribute);
-                    for(int i = 0; i < splitIndex.length; i++) {
-                        if(instance.value(m_Attribute) <= splitIndex[i]) {
+                    for(int i = 0; i < splitValues.length; i++) {
+                        if(instance.value(m_Attribute) <= splitValues[i]) {
                             index = i;
                             break;
                         }
                     }
                 } else {
-                    double value = instance.value(m_Attribute);
-                    if(value == splitIndex[0])
+                    if(instance.value(m_Attribute) == splitValues[0])
                         index = 0;
-                    else
-                        index = 1;
                 }
             } else {
                 if(m_Attribute.isNumeric()) {
-                    double value = instance.value(m_Attribute);
-                    for(int i = 0; i < splitIndex.length; i++) {
-                        if(value <= splitIndex[i]) {
+                    for(int i = 0; i < splitValues.length; i++) {
+                        if(instance.value(m_Attribute) <= splitValues[i]) {
                             index = i;
                             break;
                         }
@@ -604,7 +599,7 @@ public class Grupp3Labb1
         }
         splitData[0].compactify();
         splitData[1].compactify();
-        splitIndex[0] = splitValue;
+        splitValues[0] = splitValue;
        // this.m_Attribute = att.; denna måste sättas.... kommer inte fugnera..
         return splitData;
     }
@@ -618,11 +613,14 @@ public class Grupp3Labb1
     private Instances[] binarySplitDataNumeric(Instances data, Attribute att) {
         double maxValue = Double.NEGATIVE_INFINITY, minValue = Double.POSITIVE_INFINITY;
         Instance inst;
+        m_NumberOfSplits = 2;
 
-        Instances[] splitData = new Instances[2];
-        splitData[0] = data.stringFreeStructure();
-        splitData[1] = data.stringFreeStructure();
+        Instances[] splitData = new Instances[m_NumberOfSplits];
+        for (int i = 0; i < splitData.length; i++) {
+            splitData[i] = data.stringFreeStructure();
+        }
 
+        // Fetch min/max values in instances
         Enumeration instEnum = data.enumerateInstances();
         while (instEnum.hasMoreElements()) {
             inst = (Instance) instEnum.nextElement();
@@ -635,11 +633,12 @@ public class Grupp3Labb1
         }
 
         double diff = maxValue - minValue;
-        double splitValue = diff/2;
+        double splitValue = diff / m_NumberOfSplits;
 
-        double value = minValue+splitValue;
+        // Set distribution splitValue
+        double value = minValue + splitValue;
         for (int i = 0; i < 2; i++) {
-            splitIndex[i] = value;
+            splitValues[i] = value;
             value += splitValue;
         }
 
