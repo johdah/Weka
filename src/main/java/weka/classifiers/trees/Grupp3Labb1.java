4bbe6be18b94856061098342dee8e3174ba50f62
Administratör <loljansson@hotmail.com>
2013-02-21 23:12:29 +0100

    splitDataNumeric

--- src/main/java/weka/classifiers/trees/Grupp3Labb1.java
+++ src/main/java/weka/classifiers/trees/Grupp3Labb1.java
@@ -384,8 +384,13 @@ public class Grupp3Labb1
                 }
             }
         }
+        try{
+            return m_Successors[index].distributionForInstance(instance);
+        } catch (Exception e){
+            printDebugMessage("nullPointerExceptionMotherFucker!");
+            return m_Successors[index].distributionForInstance(instance);
+        }
 
-        return m_Successors[index].distributionForInstance(instance);
     }
 
     /**
@@ -698,74 +703,102 @@ public class Grupp3Labb1
         return splitData;
     }
 
+
+    private int getSplitInterval(int N){
+        int k;
+        //k = log2(N)
+        printDebugMessage("räknar k");
+        //k = (int)(Math.log(N) / Math.log(2)); //log2(N) fails
+        k = (int)Utils.log2(N);
+        if(k < 2)
+            k = 2;
+        printDebugMessage(String.format("%d", m_NumberOfSplits));
+        return k;
+    }
+
+
     /**
+     *
      * @param data the data which is to be split
      * @param att the attribute to be used for splitting
      * @return the sets of instances produced by the split
      */
     private Instances[] splitDataNumeric(Instances data, Attribute att) {
-        double maxValue = Double.NEGATIVE_INFINITY, minValue = Double.POSITIVE_INFINITY;
-        Instance inst;
-        splitValues = new double[m_NumberOfSplits];
-        printDebugMessage(String.format("m_NumberOfSplits: %d", m_NumberOfSplits));
+            double maxValue = Double.NEGATIVE_INFINITY, minValue = Double.POSITIVE_INFINITY;
+            int numberOfSplitInterval;
+            numberOfSplitInterval = getSplitInterval(data.numInstances());
 
-        Instances[] splitData = new Instances[m_NumberOfSplits];
-        for (int i = 0; i < splitData.length; i++) {
-            splitData[i] = data.stringFreeStructure();
-        }
+            Instance inst;
+            splitValues = new double[numberOfSplitInterval];
+            //printDebugMessage(String.format("m_NumberOfSplits: %d", m_NumberOfSplits));
 
-        // Get min-max values from instances
-        Enumeration instEnum = data.enumerateInstances();
-        while(instEnum.hasMoreElements()) {
-            inst = (Instance) instEnum.nextElement();
-            double value = inst.value(att);
+            printDebugMessage("startloop");
 
-            if(maxValue < value)
-                maxValue = value;
-            if(minValue > value)
-                minValue = value;
-        }
+            Instances[] splitData = new Instances[numberOfSplitInterval];
+            for (int i = 0; i < splitData.length; i++) {
+                splitData[i] = data.stringFreeStructure();
+            }
+            printDebugMessage("slutloop");
 
-        double diff = maxValue - minValue;
-        double splitValue = diff / m_NumberOfSplits;
+            // Get min-max values from instances
+            Enumeration instEnum = data.enumerateInstances();
+            while(instEnum.hasMoreElements()) {
+                inst = (Instance) instEnum.nextElement();
+                double value = inst.value(att);
 
-        // Set distribution
-        double value = minValue + splitValue;
-        for(int i = 0; i < m_NumberOfSplits; i++) {
-            splitValues[i] = value;
-            value += splitValue;
-        }
+                if(maxValue < value)
+                    maxValue = value;
+                if(minValue > value)
+                    minValue = value;
+            }
 
-        // Adding instances to new split attribute
-        instEnum = data.enumerateInstances();
-        while (instEnum.hasMoreElements()) {
-            inst = (Instance) instEnum.nextElement();
+            double diff = maxValue - minValue;
+            double splitValue = diff / numberOfSplitInterval;
 
-            int index = -1;
-            if(!inst.hasMissingValue()) {
-                // Inst. intervall is between min-max
-                double bound = minValue + splitValue;
+            // Set distribution
+            double value = minValue + splitValue;
+            for(int i = 0; i < numberOfSplitInterval; i++) {
+                splitValues[i] = value;
+                value += splitValue;
+            }
 
-                for(int i = 0; i < m_NumberOfSplits; i++) {
-                    if(bound < inst.value(att))
-                        bound += splitValue;
-                    else {
-                        index = i;
-                        i = m_NumberOfSplits;
+            // Adding instances to new split attribute
+            instEnum = data.enumerateInstances();
+            while (instEnum.hasMoreElements()) {
+                inst = (Instance) instEnum.nextElement();
+
+                int index = -1;
+                if(!inst.hasMissingValue()) {
+                    // Inst. intervall is between min-max
+                    double bound = minValue + splitValue;
+
+                    for(int i = 0; i < numberOfSplitInterval; i++) {
+                        if(bound < inst.value(att))
+                            bound += splitValue;
+                        else {
+                            index = i;
+                            i = numberOfSplitInterval;
+                        }
+
+                        if(i == numberOfSplitInterval-1)
+                            index = i;
                     }
-
-                    if(i == m_NumberOfSplits-1)
-                        index = i;
+                    //System.out.println(index);
+                    splitData[index].add(inst);
                 }
 
-                splitData[index].add(inst);
+
+
+
             }
-        }
 
         for (Instances aSplitData : splitData) {
             aSplitData.compactify();
         }
-
+        if(numberOfSplitInterval == 2){
+            printDebugMessage("numberOfSplitInterval =2");
+        }
+        printDebugMessage("return splitdata");
         return splitData;
     }
 
